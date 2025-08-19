47b730d52f7e54fbcb58a4a897bae652672f1b87
SnehaKapoor2004 <157450175+SnehaKapoor2004@users.noreply.github.com>
2025-08-19 17:04:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+import Contact from './components/Contact/Contact'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
