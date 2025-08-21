20bca3c3037090cbf4f26462422c76dbbc58a1e3
SnehaKapoor2004 <157450175+SnehaKapoor2004@users.noreply.github.com>
2025-08-21 17:17:21 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import './App.css'
-import Footer from './components/Footer/Footer'
-import Home from './components/Home/Home'
-import Header from './components/Header/Header'
-import Contact from './components/Contact/Contact'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    
-    </>
-=======
 import './App.css'
 import Login from './Components/Login'
 import Profile from './Components/profile'
@@ -26,7 +10,6 @@ function App() {
       <Login />
       <Profile />
     </UserContextProvider>
->>>>>>> df5dac2 (my 8th project)
   )
 }
 
